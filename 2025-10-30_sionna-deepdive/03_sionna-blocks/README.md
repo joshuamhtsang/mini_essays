8bd5874a8c5f234bc7d81fca2eda3bdfd3fa9edb
Joshua Tsang <joshua.mh.tsang@gmail.com>
2025-11-11 15:37:26 +0000

    blah

--- 2025-10-30_sionna-deepdive/03_sionna-blocks/README.md
+++ 2025-10-30_sionna-deepdive/03_sionna-blocks/README.md
@@ -5,9 +5,26 @@
 - Do simply 3-repeater code.
 - Look into automatic differentiation.
 
-# Introducing Sionna blocks
+## Introducing Sionna blocks, along with encoder and decoder for FEC
+
+A Sionna Block is a class for defining a particular comms chain. It implements two main functions:
+
+1. `__init__()` : Sionna components (`mapper`, `awgn` etc.) composing the comms chains are initialised in here.
+2. `call()` : Information bits are passed through the comms chain in here. This abstract function shold return the original generated bits and the llr, enabling the computation of statistics like BER.
+
+Consider the following comms chain, which is like the chain in [chapter 02](../02_basic-phy-layer-chain/README.md) but adds an encoder and decoder for forward error correction (FEC).
 
 ![Image](./images/sionna_chain_with_fec.png)
+
+If implemneted as a Sionna block, it looks like the schematic below:
+
+![image](./images/sionna_block.png)
+
+Instantiation of this block can be used for computing comms statistics like BER.
+
+## 
+
+
 
 
 
--- 2025-10-30_sionna-deepdive/03_sionna-blocks/main.py
+++ 2025-10-30_sionna-deepdive/03_sionna-blocks/main.py
@@ -13,8 +13,32 @@ if os.getenv("CUDA_VISIBLE_DEVICES") is None:
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+# First define a Sionna block class for an uncoded comms chain
+class UncodedAWGN(sionna.phy.Block):
+    def __init__(self, num_bits_per_symbol, block_length):
+        self.num_bits_per_symbol = num_bits_per_symbol
+        self.block_length = block_length
+        self.constellation = sionna.phy.mapping.Constellation("qam", self.num_bits_per_symbol)
+        self.mapper = sionna.phy.mapping.Mapper(constellation=self.constellation)
+        self.demapper = sionna.phy.mapping.Demapper("app", constellation=self.constellation)
+        self.binary_source = sionna.phy.mapping.BinarySource()
+        self.awgn_channel = sionna.phy.channel.AWGN()
+
+    def call(self, batch_size, ebno_db):
+        # no channel coding used; we set coderate=1.0
+        no = sionna.phy.utils.ebnodb2no(ebno_db, num_bits_per_symbol=self.num_bits_per_symbol, coderate=1.0)
+    
+        bits = self.binary_source([batch_size, self.block_length])
+        x = self.mapper(bits)
+        y = self.awgn_channel(x, no)
+        llr = self.demapper(y,no)
+
+        return bits, llr
+
 
 if __name__ == "__main__":
     print("Welcome to Chapter 03 Sionna Blocks and FEC!")
+
+    model_uncoded_awgn = UncodedAWGN(num_bits_per_symbol=2, block_length=256)
 
     
